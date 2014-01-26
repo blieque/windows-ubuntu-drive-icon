b08976bdf4b27940d3278c88260aab528b1e94c5
Blieque Mariguan <himself@blieque.co.uk>
2014-01-26 22:43:50 +0000

    More C++ development
    Continued work on the C++ installer app. It creates registry entries as
    wanted, but still needs modification, and an uninstaller implemented.

--- setup.cpp
+++ setup.cpp
@@ -1,59 +1,67 @@
 #include <iostream>
+#include <sstream>
 #include <stdlib.h>
 #include <stdio.h>
-//#include <strsafe.h>
 #include <string>
+#include <typeinfo>
 #include <windows.h>
 
 using namespace std;
 
 int main() {
-	string input [2];
+	string input[2];
+	const char* inputChar[2];
+	const char* hKeyCreate;
+	HKEY hKey = NULL;
+	HKEY hKeyOut = NULL;
 
-	cout << "Windows Ubuntu Drive Icon Setup, by Blieque Mariguan (@blieque)\nInstall or uninstall the icon? [I/U]: ";
+	cout << "\nWindows Ubuntu Drive Icon Setup, by Blieque Mariguan (@blieque)\nInstall or uninstall the icon? [I/U]: ";
 	cin >> input[0];
-
-	stringstream sstm;
-	sstm << "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\DriveIcons\\" << input[1] << "\\DefaultIcon",
-	string hKeyCreate = sstm.str();
 
 	if (input[0] == "I") {
 		cout << "Ubuntu drive letter: ";
 		cin >> input[1];
+	
+		stringstream sstm;
+		sstm << "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\DriveIcons\\" << input[1] << "\\DefaultIcon";
+		hKeyCreate = sstm.str().c_str();
+
+		inputChar[1] = input[1].c_str();
+
+		long hKeyCreator = RegCreateKeyEx(
+			HKEY_LOCAL_MACHINE,
+			hKeyCreate,
+			0,
+			NULL,
+			REG_OPTION_NON_VOLATILE,
+			KEY_ALL_ACCESS,
+			NULL,
+			&hKey,
+			NULL
+		);
+
+		long hKeySetter = RegSetValueEx(
+			HKEY_LOCAL_MACHINE,
+			hKeyCreate,
+			0,
+			REG_SZ,
+			(LPCSTR)inputChar[1],
+			2
+		);
+		/*
+	
+			int ren = rename( "ubuntu-drive.ico" , "%PUBLIC%\Pictures\ubuntu-drive.ico" );
+			if ( ren == 0 ) {
+				puts( "File successfully renamed" );
+			} else {
+				perror( "Error renaming file" );
+			}
+		*/
 	} else if (input[0] == "U") {
 		// Uninstall
 	} else {
 		// note
 	}
 
-	HKEY hKey = NULL;
-	long create = RegCreateKeyEx(
-		HKEY_LOCAL_MACHINE,
-		hKeyCreate,
-		0,
-		NULL,
-		REG_OPTION_NON_VOLATILE,
-		KEY_ALL_ACCESS,
-		NULL,
-		&hKey,
-		NULL
-	);
-/*
-	int ren = rename( "setup.rc" , "assets/setup.rc" );
-	if ( ren == 0 ) {
-		puts( "File successfully renamed" );
-	} else {
-		perror( "Error renaming file" );
-	}
-*/
 	return 0;
 }
-
-/*
-
-	result= rename( oldname , newname );
-	if ( result == 0 )
-		puts ( "File successfully renamed" );
-	else
-		perror( "Error renaming file" );
-		*/

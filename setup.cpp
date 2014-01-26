1c456a96951a37e229430957d4c74491003fa2d1
Blieque Mariguan <himself@blieque.co.uk>
2014-01-26 00:52:12 +0000

    Further development of installer
    Continued with the C++ installer app. This repo is more about the C++
    development than anything.

--- setup.cpp
+++ setup.cpp
@@ -1,21 +1,51 @@
 #include <iostream>
+#include <stdlib.h>
 #include <stdio.h>
-#include <Windows.h>
+//#include <strsafe.h>
+#include <string>
+#include <windows.h>
 
-int main() { // int argc, char* argv[]
-	LONG WINAPI RegOpenKeyEx(
-		_In_        HKEY hKey,
-		_In_opt_    LPCTSTR lpSubKey,
-		_Reserved_  DWORD ulOptions,
-		_In_        REGSAM samDesired,
-		_Out_       PHKEY phkResult
-	);
-	
-	BOOL WINAPI MoveFile(
-		_In_  LPCTSTR lpExistingFileName,
-		_In_  LPCTSTR lpNewFileName
-	);
+using namespace std;
 
+int main() {
+	string input [2];
+
+	cout << "Windows Ubuntu Drive Icon Setup, by Blieque Mariguan (@blieque)\nInstall or uninstall the icon? [I/U]: ";
+	cin >> input[0];
+
+	stringstream sstm;
+	sstm << "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\DriveIcons\\" << input[1] << "\\DefaultIcon",
+	string hKeyCreate = sstm.str();
+
+	if (input[0] == "I") {
+		cout << "Ubuntu drive letter: ";
+		cin >> input[1];
+	} else if (input[0] == "U") {
+		// Uninstall
+	} else {
+		// note
+	}
+
+	HKEY hKey = NULL;
+	long create = RegCreateKeyEx(
+		HKEY_LOCAL_MACHINE,
+		hKeyCreate,
+		0,
+		NULL,
+		REG_OPTION_NON_VOLATILE,
+		KEY_ALL_ACCESS,
+		NULL,
+		&hKey,
+		NULL
+	);
+/*
+	int ren = rename( "setup.rc" , "assets/setup.rc" );
+	if ( ren == 0 ) {
+		puts( "File successfully renamed" );
+	} else {
+		perror( "Error renaming file" );
+	}
+*/
 	return 0;
 }
 
